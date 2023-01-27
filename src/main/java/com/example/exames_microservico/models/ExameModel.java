88b187707a46dd5e7cfec89d5ea567bcc2084817
Fellyx <Fellyx@users.noreply.github.com>
2023-01-27 11:23:08 -0300

    Adicionando o Campos do model Exame Adicionando os campos do model Exame e criando o repository.

--- src/main/java/com/example/exames_microservico/models/ExameModel.java
+++ src/main/java/com/example/exames_microservico/models/ExameModel.java
@@ -1,5 +1,49 @@
 package com.example.exames_microservico.models;
 
-public class ExameModel {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_EXAME")
+public class ExameModel implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID cd_exame;
+	@Column(nullable = false, unique = true, length = 10)
+	private String nome;
+	@Column(nullable = false, unique = true, length = 250)
+	private String descricao;
+	
+	
+	public UUID getCd_exame() {
+		return cd_exame;
+	}
+	
+	public void setCd_exame(UUID cd_exame) {
+		this.cd_exame = cd_exame;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+	
 
 }
---
+++ src/main/java/com/example/exames_microservico/repositories/ExameRepository.java
@@ -0,0 +1,12 @@
+package com.example.exames_microservico.repositories;
+
+import com.example.exames_microservico.models.ExameModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ExameRepository extends JpaRepository<ExameModel, UUID> {
+
+}
