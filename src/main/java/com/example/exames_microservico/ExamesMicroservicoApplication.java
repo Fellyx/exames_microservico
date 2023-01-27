98858351bd59f02b45306e0b878af958d6d6b606
Fellyx <Fellyx@users.noreply.github.com>
2023-01-27 09:11:44 -0300

    first commit

---
+++ src/main/java/com/example/exames_microservico/ExamesMicroservicoApplication.java
@@ -0,0 +1,13 @@
+package com.example.exames_microservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamesMicroservicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamesMicroservicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exames_microservico/ExamesMicroservicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exames_microservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamesMicroservicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
